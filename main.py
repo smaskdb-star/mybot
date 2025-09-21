1bb879afdb607f20003937776af83db62db08537
smaskdb-star <smaskdb@gmail.com>
2025-09-21 15:26:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from keep_alive import keep_alive
+import time
+
+# إبقاء البوت شغال
+keep_alive()
+
+# هنا كود البوت مالتك (حاليا مثال بسيط)
+while True:
+    print("Bot is running...")
+    time.sleep(10)
